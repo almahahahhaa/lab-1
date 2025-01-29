a26eeac927e535cf7b788440140869c51df20e99
almahahahhaa <98401746+almahahahhaa@users.noreply.github.com>
2025-01-29 12:35:45 +0400

    Add files via upload

---
+++ variables4.rs
@@ -0,0 +1,8 @@
+fn main() {
+    
+    let mut x: i32;
+
+    x = 42;
+
+    println!("Number {x}");
+}
---
+++ variables5.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let number = "T-H-R-E-E"; 
+    println!("Spell a number: {}", number);
+
+   
+    let mut number = 3;
+    println!("Number plus two is: {}", number + 2);
+}
