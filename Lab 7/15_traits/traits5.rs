f5a3582e6eb6e4af9c14c466daa1362151f1832a
almahahahhaa <98401746+almahahahhaa@users.noreply.github.com>
2025-03-11 10:47:50 +0400

    Add files via upload

---
+++ Lab 7/15_traits/traits5.rs
@@ -0,0 +1,39 @@
+trait SomeTrait {
+    fn some_function(&self) -> bool {
+        true
+    }
+}
+
+trait OtherTrait {
+    fn other_function(&self) -> bool {
+        true
+    }
+}
+
+struct SomeStruct;
+impl SomeTrait for SomeStruct {}
+impl OtherTrait for SomeStruct {}
+
+struct OtherStruct;
+impl SomeTrait for OtherStruct {}
+impl OtherTrait for OtherStruct {}
+
+// Use a generic type parameter with a trait bound that ensures the item implements both traits.
+fn some_func<T: SomeTrait + OtherTrait>(item: T) -> bool {
+    item.some_function() && item.other_function()
+}
+
+fn main() {
+    // You can optionally experiment here.
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_some_func() {
+        assert!(some_func(SomeStruct));
+        assert!(some_func(OtherStruct));
+    }
+}
