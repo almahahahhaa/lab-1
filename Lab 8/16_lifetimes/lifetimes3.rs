49c6e56a5f617b8e0449e51a8dd67be156f253f9
almahahahhaa <98401746+almahahahhaa@users.noreply.github.com>
2025-03-18 10:06:25 +0400

    Add files via upload

---
+++ Lab 8/16_lifetimes/lifetimes3.rs
@@ -0,0 +1,13 @@
+struct Book<'a> {
+    author: &'a str,
+    title: &'a str,
+}
+
+fn main() {
+    let book = Book {
+        author: "George Orwell",
+        title: "1984",
+    };
+
+    println!("{} by {}", book.title, book.author);
+}
