47d785b0031ef27430e895f842b318a809ed70b9
almahahahhaa <98401746+almahahahhaa@users.noreply.github.com>
2025-04-08 10:01:55 +0400

    Add files via upload

---
+++ Lab 9/iterators5.rs
@@ -0,0 +1,41 @@
+use std::collections::HashMap;
+
+#[derive(Clone, Copy, PartialEq, Eq)]
+enum Progress {
+    None,
+    Some,
+    Complete,
+}
+
+fn count_for(map: &HashMap<String, Progress>, value: Progress) -> usize {
+    let mut count = 0;
+    for val in map.values() {
+        if *val == value {
+            count += 1;
+        }
+    }
+    count
+}
+
+fn count_iterator(map: &HashMap<String, Progress>, value: Progress) -> usize {
+    map.values().filter(|&v| *v == value).count()
+}
+
+fn count_collection_for(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
+    let mut count = 0;
+    for map in collection {
+        for val in map.values() {
+            if *val == value {
+                count += 1;
+            }
+        }
+    }
+    count
+}
+
+fn count_collection_iterator(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
+    collection.iter()
+        .flat_map(|map| map.values())
+        .filter(|&v| *v == value)
+        .count()
+}
